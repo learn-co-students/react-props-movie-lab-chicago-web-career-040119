4292f92a5e5382588d4f7b94e3aa9724792505c2
Sonia Dumitru <soni.dumitru@gmail.com>
2019-06-03 18:15:33 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,12 +1,24 @@
+//MovieShowcase is the component that will house all raw data assoc with the movies we want to display
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import movieData from './data.js' //import all info about each movie
 
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    return movieData.map((movie) =>
+        <MovieCard
+          title = {movie.title}
+          rating = {movie.IMDBRating}
+          genres = {movie.genres}
+          poster = {movie.poster}
+          />)
   }
+//just to pass the tests
+  // generateMovieCards = () => {
+  //   return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+  // }
+
 
   render() {
     return (
